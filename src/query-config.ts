ea4c8c9e96c03436ee98e930e4642fae6901c579
Kalana Vithana <kalana@propps.com>
2023-01-24 11:42:45 +1100

    feat: add return type to config

--- src/query-config.ts
+++ src/query-config.ts
@@ -1,15 +1,12 @@
 import { UseQueryOptions } from "@tanstack/react-query"
 
-export const DEFAULT_QUERY_CONFIG: Omit<
-  UseQueryOptions<any, any, any, string[]>,
-  "queryKey" | "queryFn" | "initialData"
-> = {
+export const DEFAULT_QUERY_CONFIG: UseQueryOptions = {
   refetchOnMount: false,
   refetchOnReconnect: false,
   refetchOnWindowFocus: false,
   staleTime: Infinity,
 }
 
-export const config = <T>(config?: UseQueryOptions<any, any, T, string[]>) => {
+export const config = <T>(config?: UseQueryOptions<any, any, T, string[]>): UseQueryOptions<any, any, T, string[]> => {
   return Object.assign({ ...DEFAULT_QUERY_CONFIG }, config)
 }
