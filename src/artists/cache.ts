e8af7447495a0962d580f517ee456d04951e9c5e
Kalana Vithana <kalana@propps.com>
2023-01-27 12:36:15 +1100

    feat: smarter caching of artists and tracks

--- src/artists/cache.ts
+++ src/artists/cache.ts
@@ -1,7 +1,13 @@
 import { QueryClient } from "@tanstack/react-query"
 
-export function addArtistsToCache(client: QueryClient, artists: SpotifyApi.ArtistObjectSimplified[]) {
+export function addArtistsToCache(
+  client: QueryClient,
+  artists: SpotifyApi.ArtistObjectSimplified[] | SpotifyApi.ArtistObjectFull[]
+) {
   for (const artist of artists) {
+    if ("genres" in artist) {
+      client.setQueryData(["artist", "full", artist.id], artist)
+    }
     client.setQueryData(["artist", "simplified", artist.id], artist)
   }
 }
--- src/tracks/cache.ts
+++ src/tracks/cache.ts
@@ -1,7 +1,14 @@
 import { QueryClient } from "@tanstack/react-query"
 
-export function addTracksToCache(client: QueryClient, tracks: SpotifyApi.TrackObjectSimplified[]) {
+export function addTracksToCache(client: QueryClient, tracks: SpotifyApi.TrackObjectSimplified[] | SpotifyApi.TrackObjectFull[]) {
   for (const track of tracks) {
+    if ('album' in track) {
+      client.setQueryData(["album", "simplified", track.album.id], track.album)
+      for (const artist of track.artists) {
+      client.setQueryData(["artist", "simplified", artist.id], artist)
+      }
+      client.setQueryData(["track", "full", track.id], track)
+    }
     client.setQueryData(["track", "simplified", track.id], track)
   }
 }
